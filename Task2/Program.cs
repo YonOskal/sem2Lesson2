ac034909a5b41957f610fabba98f8d6edce92cc2
Yon <larin.andreika@yandex.ru>
2024-05-01 16:02:48 +0300

    отредактировали таск2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,28 +1,21 @@
-﻿// Напишите программу, которая принимает на вход трёхзначное
-// число и возводит вторую цифру этого числа в степень, равную
-// третьей цифре.
-// Примеры
-// 487 => 8^7 = 2 097 152
-// 254 => 5^4 = 625
-// 617 => 1
-
-Console.WriteLine("Введите трехзначное число: ");
+﻿Console.WriteLine("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-
+int SecondDigit = 0;
+int ThirdDigit = 0;
 if (num >= 100 && num <= 999)
 {
-    int SecondDigit = num / 10 % 10;
-    int ThirdDigit = num % 10;
-    int result = SecondDigit;
+    SecondDigit = num / 10 % 10;
+    ThirdDigit = num % 10;
+}
+else
+{
+    Console.WriteLine("Некоректный ввод");
+}
+int result = SecondDigit;
 int count = 1;
 while (count < ThirdDigit)
 {
     result *= SecondDigit; //result = result * SecondDigit
     count++;
 }
-Console.WriteLine(result);
-}
-else
-{
-    Console.WriteLine("Некоректный ввод");
-}
+ Console.WriteLine(result);
