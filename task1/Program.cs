013c0699f922bdf450e69f2536cd7eb5a5b0a416
Yon <larin.andreika@yandex.ru>
2024-05-01 14:46:05 +0300

    добавили задачу в таск1

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите целое положительное трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999)
+{
+int firstDigit = num / 100; // 256 / 100 = 2.56 = 2
+int thirdDigit = num % 10; // 256 % 10 = 250 + 6 = 6
+
+int result = firstDigit * 10 + thirdDigit;
+
+Console.WriteLine(result);
+
+}
+else
+{
+Console.WriteLine("Некорректный ввод!");
+}
