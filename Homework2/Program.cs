19d0ebf502897c1660e45069d4ecc7ab7015753e
Yon <larin.andreika@yandex.ru>
2024-05-03 20:50:01 +0300

    add Homework3 Sem2

---
+++ Homework2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход 
+// координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер 
+// координатной четверти плоскости, в которой находится эта точка.
+
+Console.WriteLine("Введите число X: ");
+int X = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Введите число Y: ");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if (X > 0 && Y > 0)
+{
+    Console.WriteLine("Первая четверть координат");
+}
+else if (X < 0 && Y > 0)
+{
+    Console.WriteLine("Вторая четверть координат");
+}
+else if (X < 0 && Y < 0)
+{
+    Console.WriteLine("Третья четверть координат");
+}
+else if (X > 0 && Y < 0)
+{
+    Console.WriteLine("Четвертая четверть координат");
+}
+else
+{
+    Console.WriteLine("Некоректный ввод координат");
+}
---
+++ Homework3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа
+
+Console.WriteLine("Введите двухзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num1 = 0;
+int num2 = 0;
+
+if (num >= 10 && num <= 99)
+{
+    num1 = num / 10;
+    num2 = num % 10;
+    
+    if (num1 > num2)
+{
+    Console.WriteLine("Наибольшее число " + num1);
+}
+    else if (num2 > num1)
+{
+    Console.WriteLine("Наибольшее число " + num2);
+}
+ else
+{
+    Console.WriteLine("Числа равны");
+}
+}
+else
+{
+    Console.WriteLine("Некоректный ввод");
+}
