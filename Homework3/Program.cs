0cebe64783dde6d871062b1fdec2f92038ef0620
Yon <larin.andreika@yandex.ru>
2024-05-23 01:59:28 +0300

    :wq

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -2,29 +2,30 @@
 
 Console.WriteLine("Введите двухзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
+{
+    int num1 = 0;
+    int num2 = 0;
 
-int num1 = 0;
-int num2 = 0;
+    if (num >= 10 && num <= 99)
+    {
+        num1 = num / 10;
+        num2 = num % 10;
 
-if (num >= 10 && num <= 99)
-{
-    num1 = num / 10;
-    num2 = num % 10;
-    
-    if (num1 > num2)
-{
-    Console.WriteLine("Наибольшее число " + num1);
-}
-    else if (num2 > num1)
-{
-    Console.WriteLine("Наибольшее число " + num2);
-}
- else
-{
-    Console.WriteLine("Числа равны");
-}
-}
-else
-{
-    Console.WriteLine("Некоректный ввод");
+        if (num1 > num2)
+        {
+            Console.WriteLine("Наибольшее число " + num1);
+        }
+        else if (num2 > num1)
+        {
+            Console.WriteLine("Наибольшее число " + num2);
+        }
+        else
+        {
+            Console.WriteLine("Числа равны");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Некоректный ввод");
+    }
 }
